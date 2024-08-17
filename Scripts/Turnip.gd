17b884144551e91d42ce8e31c6c3f7a70cee9739
XBenichi <77522691+XBenichi@users.noreply.github.com>
2024-08-17 18:12:47 +0300

    lil bro can move now

--- Scripts/Turnip.gd
+++ Scripts/Turnip.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
+export var speed = 100
 export var size = 1
+
 
 
 
@@ -10,13 +12,13 @@ var state = States.IDLE
 
 var max_size = 5
 var throw_height = -40
-
+var runAway = false
+var targetPosition = Vector2.ZERO
 
 
 onready var anchor = $Anchor
 onready var animationPlayer = $AnimationPlayer
 onready var tween = $Tween
-
 
 func _ready():
 	pass
@@ -26,17 +28,19 @@ func _physics_process(delta):
 		States.IDLE:
 			idle_state()
 		States.MOVING:
-			move_state()
+			move_state(delta)
 		States.HELD:
 			held_state()
 		States.THROWN:
 			thrown_state()
+	
+	position = position.round()
 
 func idle_state():
 	animationPlayer.play("Idle" + str(size))
 
-func move_state():
-	pass
+func move_state(delta):
+	move_towards_target()
 
 func held_state():
 	global_position = global.player.CarryPosition.global_position
@@ -49,6 +53,7 @@ func set_state(newState):
 
 func grow():
 	size += 1
+	speed = round(150 / (1 + (size - 1) * 0.5))
 
 func throw(newPos, time):
 	animationPlayer.play("Throw" + str(size))
@@ -74,6 +79,7 @@ func die():
 func land():
 	$Anchor/Eatbox/CollisionShape2D.disabled = true
 	set_state(States.IDLE)
+	$MoveTime.start(0.5)
 
 func set_held():
 	set_state(States.HELD)
@@ -91,3 +97,88 @@ func _on_Absorber_area_entered(area):
 		var driplet = area.get_parent().get_parent()
 		driplet.die()
 		grow()
+
+
+
+
+func choose_random_position():
+	
+	if state == States.HELD:
+		return # if this little [insert slur here] is behind held, he stops moving
+	
+	var shape = $WanderRadius/CollisionShape2D.shape
+	
+	var radius = shape.radius
+	var randomAngle = randf() * PI * 2
+	var randomRadius = randf() * radius
+	
+	var offset = Vector2(cos(randomAngle), sin(randomAngle)) * randomRadius
+	targetPosition = self.position + offset
+	
+	
+	var rng = RandomNumberGenerator.new()
+	
+	# if targetPosition is so close to the current position then offset target position by 16
+	if position.distance_to(targetPosition) < 8:
+		
+		var newPos = Vector2(16 * rng.randi_range(-1,1), 16 * rng.randi_range(-1,1))
+		
+		targetPosition += newPos
+	
+
+	set_state(States.MOVING)
+	
+
+func _on_MoveTime_timeout():
+	choose_random_position()
+
+
+func move_towards_target():
+	var direction: Vector2
+	
+	animationPlayer.play("Walk" + str(size))
+	
+	var spd = speed
+	
+	if runAway:
+		animationPlayer.playback_speed = 2
+		direction = (position - global.player.position).normalized()
+		spd = speed * 1.2
+		targetPosition = position + direction * ($FleeArea/CollisionShape2D.shape.radius * 2)  # RUN BITCH
+	
+	else:
+		animationPlayer.playback_speed = 1.5
+		spd = speed
+		direction = (targetPosition - position).normalized()
+	
+	
+	
+	if direction.round().x == -1 or direction.round().y == -1:
+		$Anchor/Sprite.flip_h = true
+	else:
+		$Anchor/Sprite.flip_h = false
+
+	var movement = direction * spd * get_process_delta_time()
+
+	if position.distance_to(targetPosition) > movement.length():
+		position += movement
+	else:
+		position = targetPosition
+		set_state(States.IDLE)
+		var rng = RandomNumberGenerator.new()
+		$MoveTime.start(rng.randf_range(0.5,2))
+		
+
+
+
+
+
+func _on_FleeArea_body_entered(body):
+	if body == global.player:
+		runAway = true
+		choose_random_position()
+
+
+func _on_SafeArea_body_exited(body):
+	if body == global.player:
+		runAway = false  # stop acting like a pussy
