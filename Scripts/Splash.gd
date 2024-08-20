2ed1441355a79e0ef9ef27777c213e051bf9175e
Pkdotts <43733267+Pkdotts@users.noreply.github.com>
2024-08-20 01:50:56 -0400

    added F5 + F4 buttons + added splash skip

--- Scripts/Splash.gd
+++ Scripts/Splash.gd
@@ -1,21 +1,14 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	yield(get_tree().create_timer(0.1),"timeout")
 	$AnimationPlayer.play("start")
 
+func _input(event):
+	if event.is_action_pressed("left_click"):
+		_on_AnimationPlayer_animation_finished("start")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
-
-func _on_AnimationPlayer_animation_finished(anim_name):
+func _on_AnimationPlayer_animation_finished(_anim_name):
 	global.change_scenes("res://Maps/TutorialFarm.tscn",2)
